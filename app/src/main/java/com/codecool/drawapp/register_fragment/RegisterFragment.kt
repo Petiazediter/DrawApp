d8eb7de905cf1d5888e78b42cd57a6ed073df2db
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-09-24 17:59:15 +0200

    Register fragment init

---
+++ app/src/main/java/com/codecool/drawapp/register_fragment/RegisterFragment.kt
@@ -0,0 +1,15 @@
+package com.codecool.drawapp.register_fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.codecool.drawapp.R
+
+
+class RegisterFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+}
