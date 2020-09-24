789f43727b08ffc0eb8ae576f5c003ac54fadd81
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-09-24 12:00:17 +0200

    Pencil icon added

--- app/src/main/java/com/codecool/drawapp/loading_fragment/LoadingFragment.kt
+++ app/src/main/java/com/codecool/drawapp/loading_fragment/LoadingFragment.kt
@@ -1,12 +1,14 @@
-package com.codecool.drawapp
+package com.codecool.drawapp.loading_fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.codecool.drawapp.R
 
 class LoadingFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_loading, container, false)
     }
@@ -14,5 +16,4 @@ class LoadingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
-
 }
