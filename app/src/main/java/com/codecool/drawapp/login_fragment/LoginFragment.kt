60a1bd6eb9aac334fffd3f675620f1253d5117f9
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-09-24 19:05:44 +0200

    [Redesign] Whole app redesign

--- app/src/main/java/com/codecool/drawapp/login_fragment/LoginFragment.kt
+++ app/src/main/java/com/codecool/drawapp/login_fragment/LoginFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.codecool.drawapp.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
 
@@ -15,6 +17,8 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        sign_up_button.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
     }
 }
--- app/src/main/java/com/codecool/drawapp/register_fragment/RegisterFragment.kt
+++ app/src/main/java/com/codecool/drawapp/register_fragment/RegisterFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.codecool.drawapp.R
+import kotlinx.android.synthetic.main.fragment_register.*
 
 
 class RegisterFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sign_in_button.setOnClickListener { findNavController().navigate(R.id.action_registerFragment_to_loginFragment) }
     }
 }
