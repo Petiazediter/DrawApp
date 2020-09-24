c8375bf2cc4c53980707cf97c587556a94bdf790
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-09-24 14:12:29 +0200

    Light action bar

--- app/src/main/java/com/codecool/drawapp/loading_fragment/LoadingFragment.kt
+++ app/src/main/java/com/codecool/drawapp/loading_fragment/LoadingFragment.kt
@@ -1,11 +1,16 @@
 package com.codecool.drawapp.loading_fragment
 
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import com.codecool.drawapp.R
+import com.jaredrummler.android.widget.AnimatedSvgView
+import kotlinx.android.synthetic.main.fragment_loading.view.*
 
 class LoadingFragment : Fragment() {
 
@@ -13,6 +18,7 @@ class LoadingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_loading, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
